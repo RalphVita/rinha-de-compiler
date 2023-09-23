d6170e36fad54738ac1b1de071b64de451b9e7d2
Wanderson Ralph <ralph.vita@gmail.com>
2023-09-23 02:02:52 -0300

    Tebela de simbolos

---
+++ rinha-compiler/include/symbol_table.hpp
@@ -0,0 +1,47 @@
+#include <map>
+#include <string>
+#include <iostream>
+#include <memory>
+
+#ifndef SYMBOL_TABLE_HPP
+#define SYMBOL_TABLE_HPP
+struct Symbol{
+    int index;
+};
+
+class SymbolTable
+{
+private:
+    std::unique_ptr<SymbolTable> parent;
+    std::map<std::string, Symbol> table;
+public:
+    SymbolTable(std::unique_ptr<SymbolTable> parent);
+    void Put(std::string id, Symbol simbol);
+    Symbol Get(std::string id);
+    ~SymbolTable();
+};
+
+SymbolTable::SymbolTable(std::unique_ptr<SymbolTable> parent)
+{
+    this->parent = std::move(parent);   
+}
+
+Symbol SymbolTable::Get(std::string id){
+    for(std::unique_ptr<SymbolTable> st = this; st != nullptr; st->parent){
+        auto simbol = st->table.find(id);
+        if(simbol != st->table.end())
+            return simbol->second;
+    }
+    throw 555;
+}
+
+void SymbolTable::Put(std::string id, Symbol simbol){
+    table.insert({id, simbol});
+}
+
+SymbolTable::~SymbolTable()
+{
+}
+
+
+#endif
---
+++ rinha-compiler/src/symbol_table.cpp
@@ -0,0 +1,10 @@
+#include <symbol_table.hpp>
+#include <iostream>
+#include <memory>
+#ifndef SYMBOL_TABLE_HPP
+#define SYMBOL_TABLE_HPP
+
+
+
+
+#endif
