5995bdea2191742289ad23c9eb6a6c57e060cd8a
Wanderson Ralph <ralph.vita@gmail.com>
2023-09-27 08:10:59 -0300

    Refatorando interpretador de funções

--- rinha-compiler/src/interpreter_function.cpp
+++ rinha-compiler/src/interpreter_function.cpp
@@ -10,43 +10,31 @@ extern rinha_compiler::SymbolTable* symbolTable;
 extern int current_scope;
 
 namespace rinha_compiler::walker {
+    rinha_compiler::Symbol get_symbol_function(box<Call>&);
+    void load_arguments_onto_stack(box<Call>&);
+    void create_page_memory(int);
+    void delete_page_memory(int);
+
     void run_call(box<Call>& term){
         int saved_scope = current_scope;
 
-        //Carrega argumentos na pilha
-        int arity = term->arguments.terms.size();
-        if(arity > 0){
-            for(auto arg: term->arguments.terms)
-                std::visit(walker::VisitTerm{}, arg);
-        }
+        load_arguments_onto_stack(term);
 
-        //Recupera nome da função
-        std::visit(walker::VisitTerm{}, term->callee.terms.front());
-        Type function_name = _stack.pop();
-        if(!std::holds_alternative<std::string>(function_name))
-            throw rinha_compiler::RinhaException("Nome de função inválido.", term->location);
-        
-        //Recupera função da tabela de simbolos
-        std::string id = std::get<std::string>(function_name);
-        rinha_compiler::Symbol symbol = symbolTable->Get(id);
+        auto symbol = get_symbol_function(term);
         
         //Muda de escopo
         //A função sempre é salva com o espopo da parent. Então o novo escopo é:
         current_scope = symbol.scope + 1;
 
-        //Aloca mas espaço na memória, para o novo escopo
-        _memory.increment(current_scope + 1);
-        //Cria página de memória
-        _memory.push(current_scope);
+        create_page_memory(current_scope);
         
-        //Busca e aponta para o bloco da função na AST
+        //Aponta para o bloco da função na AST
         auto let_function = std::get<box<Let>>(symbol.term);
 
         //Varre o bloco da função recursivamente
         std::visit(walker::VisitTerm{}, let_function->value.terms.front());
 
-        //Deleta página de memória
-        _memory.pop(current_scope);
+        delete_page_memory(current_scope);
 
         current_scope = saved_scope;
     }
@@ -87,6 +75,39 @@ namespace rinha_compiler::walker {
 
             _memory.store(symbol, value);
         }
+    }
+
+    rinha_compiler::Symbol get_symbol_function(box<Call>& term){
+        //Recupera nome da função
+        std::visit(walker::VisitTerm{}, term->callee.terms.front());
+        Type function_name = _stack.pop();
+        if(!std::holds_alternative<std::string>(function_name))
+            throw rinha_compiler::RinhaException("Nome de função inválido.", term->location);
+        
+        //Recupera função da tabela de simbolos
+        std::string id = std::get<std::string>(function_name);
+        rinha_compiler::Symbol symbol = symbolTable->Get(id);
+
+        return symbol;
+    }
+
+    void load_arguments_onto_stack(box<Call>& term){
+        int arity = term->arguments.terms.size();
+        if(arity > 0){
+            for(auto arg: term->arguments.terms)
+                std::visit(walker::VisitTerm{}, arg);
+        }
+    }
+
+    void create_page_memory(int current_scope){
+        //Aloca mas espaço na memória, para o novo escopo
+        _memory.increment(current_scope + 1);
+        //Cria página de memória
+        _memory.push(current_scope);
+    }
+    void delete_page_memory(int current_scope){
+        //Deleta página de memória
+        _memory.pop(current_scope);
     }
 
 }
